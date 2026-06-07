2827d3631cdb57a561f3cb94bd2b2d87b261e6f3
ishowsagar <228666651+ishowsagar@users.noreply.github.com>
2026-06-07 14:55:58 +0530

    changed env config

--- Backend/utils/env.go
+++ Backend/utils/env.go
@@ -11,35 +11,35 @@ import (
 )
 
 type ENVConfig struct {
-	DbHost string
-	DbUser string
-	DbPass string
-	DbPort int
-	DbName string
-	ServerPort string
-	JwtSecret string
-	RedisDB int
+	DbHost          string
+	DbUser          string
+	DbPass          string
+	DbPort          int
+	DbName          string
+	ServerPort      string
+	JwtSecret       string
+	RedisDB         int
 	RedisDBPassword string
-	RedisHost string 
-	RabbitMQURL string
-	S3AccessKeyID string
-	S3SecretKey string
-	S3BucketName string
-	S3RegionName string
+	RedisHost       string
+	RabbitMQURL     string
+	S3AccessKeyID   string
+	S3SecretKey     string
+	S3BucketName    string
+	S3RegionName    string
 }
 
-var once sync.Once 
+var once sync.Once
 var config *ENVConfig // var that stores type of ENVConfig
 // returns instance of type Config -> which stores all env variables
-func LoadConfig() (*ENVConfig,error) {
+func LoadConfig() (*ENVConfig, error) {
 
 	// load env
 	err := godotenv.Load()
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
-	//** Note - since our app is pulling var from local env file which is inaccessible to the docker containers, so we had to define matching variables in yml so it pulls from yml env **// 
+	//** Note - since our app is pulling var from local env file which is inaccessible to the docker containers, so we had to define matching variables in yml so it pulls from yml env **//
 	// access env protected variables
 	dbUSER := os.Getenv("DB_USER")
 	dbPASS := os.Getenv("DB_PASSWORD")
@@ -53,21 +53,21 @@ func LoadConfig() (*ENVConfig,error) {
 	rabbitmqURL := os.Getenv("RABBITMQ_URL")
 
 	// * since we stored aws s3 important keys in env, container would look for them in its space <- must define there too
-	s3AccessKeyID := os.Getenv("S3_ACCESS_KEY_ID")
-	s3SecretKey := os.Getenv("S3_SECRET_ACCESS_KEY")
+	s3AccessKeyID := os.Getenv("AwsAccessKeyID")
+	s3SecretKey := os.Getenv("AwsAccessSecretKey")
 	s3BucketName := os.Getenv("S3BucketName") // switching to what aws expects naming to be for default loads
-	s3RegionName := os.Getenv("S3_REGION")
+	s3RegionName := os.Getenv("AwsRegion")
 
 	dbPort, err := strconv.Atoi(dbPortStr)
 	if err != nil {
 		slog.Error("failed to get port", "error", err)
-		return nil,err
+		return nil, err
 	}
 
 	redisDb, err := strconv.Atoi(redisDbStr)
 	if err != nil {
 		slog.Error("failed to get redis db", "error", err)
-		return nil,err
+		return nil, err
 	}
 	// port,err := strconv.Atoi(serverPortStr)
 	// if err != nil {
@@ -77,44 +77,49 @@ func LoadConfig() (*ENVConfig,error) {
 
 	dbName := os.Getenv("DB_DBASE_NAME")
 
-
 	// env check
 	fmt.Println("--- DOCKER ENV CHECK ---")
-	fmt.Printf("Access ID:  '%s'\n", os.Getenv("S3_ACCESS_KEY_ID"))
-	fmt.Printf("Secret Key: '%s' (Length: %d)\n", os.Getenv("S3_SECRET_ACCESS_KEY"), len(os.Getenv("S3_SECRET_ACCESS_KEY")))
+	fmt.Printf("New Access ID:  '%s'\n", os.Getenv("AwsAccessKeyID"))
+	fmt.Printf("New Secret Key: '%s' (Length: %d)\n", os.Getenv("AwsAccessSecretKey"), len(os.Getenv("AwsAccessSecretKey")))
 	fmt.Printf("Bucket:     '%s'\n", os.Getenv("S3BucketName"))
-	fmt.Printf("Region:     '%s'\n", os.Getenv("S3_REGION"))
+	fmt.Printf("New Region:     '%s'\n", os.Getenv("AwsRegion"))
 	fmt.Println("------------------------")
+
+	// KER ENV CHECK ---")
+	// fmt.Printf("Access ID:  '%s'\n", os.Getenv("S3AccessKeyID"))
+	// fmt.Printf("Secret Key: '%s' (Length: %d)\n", os.Getenv("S3SecretKey"), len(os.Getenv("AWS_SECRET_ACCESS_KEY")))
+	// fmt.Printf("Bucket:     '%s'\n", os.Getenv("S3BucketName"))
+	// fmt.Printf("Region:     '%s'\n", os.Getenv("S3Region"))
+	// fmt.Println("---------------------
 
 	// returning instance with env accessed vars
 	return &ENVConfig{
-		DbHost: dbHOST,
-		DbUser: dbUSER,
-		DbPass: dbPASS,
-		DbPort: dbPort,
-		DbName: dbName,
-		ServerPort: serverPortStr,
-		JwtSecret:JwtSecretKey,
-		RedisDB: redisDb,
+		DbHost:          dbHOST,
+		DbUser:          dbUSER,
+		DbPass:          dbPASS,
+		DbPort:          dbPort,
+		DbName:          dbName,
+		ServerPort:      serverPortStr,
+		JwtSecret:       JwtSecretKey,
+		RedisDB:         redisDb,
 		RedisDBPassword: redisDbPass,
-		RedisHost: redisDbHost,
-		RabbitMQURL: rabbitmqURL,
-		S3AccessKeyID: s3AccessKeyID,
-		S3SecretKey: s3SecretKey,
-		S3BucketName: s3BucketName,
-		S3RegionName: s3RegionName,
-	},nil
+		RedisHost:       redisDbHost,
+		RabbitMQURL:     rabbitmqURL,
+		S3AccessKeyID:   s3AccessKeyID,
+		S3SecretKey:     s3SecretKey,
+		S3BucketName:    s3BucketName,
+		S3RegionName:    s3RegionName,
+	}, nil
 }
-
 
 // load config type's instance all in once for use
 func GetConfig() *ENVConfig {
 	once.Do(func() {
-		loadedConfig,err := LoadConfig()
+		loadedConfig, err := LoadConfig()
 		if err != nil {
 			panic(err)
 		}
-		config = loadedConfig //* setting var to assign this struct instance values 
+		config = loadedConfig //* setting var to assign this struct instance values
 	})
-	return  config
+	return config
 }
